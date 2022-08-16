286f1da28c75f9c8247e1306bd1d8587c397558c
mumucang <642138438@qq.com>
2022-08-16 11:57:20 +0800

    xiangmu

---
+++ chapter5/src/main/java/com/course/test/BaseAnnotation.java
@@ -0,0 +1,15 @@
+package com.course.test;
+
+import org.testng.annotations.Test;
+
+public class BaseAnnotation {
+
+    @Test
+    public void testCase1(){
+        System.out.println("ceshi1");
+
+
+    }
+
+
+}
